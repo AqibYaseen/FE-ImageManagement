5b97e8a3f9734ea595bcb4964944979726c5dda7
AqibYaseen <aqibyaseenbhat@gmail.com>
2024-06-21 21:02:27 +0530

    fixed empty route to redirect to images module, placeholder if no images are available

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'image', redirectTo: '/image', pathMatch: 'full' },
+  { path: '', redirectTo: '/image', pathMatch: 'full' },
   {
     path: 'image',
     loadChildren: () =>
